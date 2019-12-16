9c9efa609ea3c5acd632bf4be0b421905cfbb77d
Will Foster <forest_5592@hotmail.co.uk>
2019-12-16 13:53:00 +0000

    Updates

---
+++ precache-manifest.844a3a9a3dff174acc6f7aac27839ac3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efd8646c2b3ab65aad3065aa8afd92e",
+    "url": "/react-color-generator/index.html"
+  },
+  {
+    "revision": "2ff70ad5be82081ba490",
+    "url": "/react-color-generator/static/css/main.82f1a892.chunk.css"
+  },
+  {
+    "revision": "b4c6a59a975f8d6556f7",
+    "url": "/react-color-generator/static/js/2.2aab25e2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-color-generator/static/js/2.2aab25e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ff70ad5be82081ba490",
+    "url": "/react-color-generator/static/js/main.116a3d26.chunk.js"
+  },
+  {
+    "revision": "497da36d1d0e99abb37d",
+    "url": "/react-color-generator/static/js/runtime-main.eebe34cc.js"
+  },
+  {
+    "revision": "35a6ccb5ed555dd6c31878ae02c9fd0d",
+    "url": "/react-color-generator/static/media/error.35a6ccb5.svg"
+  },
+  {
+    "revision": "6d15d93a6c4b20001d43c5ecb867e28a",
+    "url": "/react-color-generator/static/media/right-arrow.6d15d93a.svg"
+  }
+]);
